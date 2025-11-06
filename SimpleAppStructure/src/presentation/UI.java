1c251612f9fa0f24bab63e14088be7eafa2aad24
Troels Mortensen <tromortensen@hotmail.com>
2025-11-06 13:40:38 +0100

    something in the ui

--- SimpleAppStructure/src/presentation/UI.java
+++ SimpleAppStructure/src/presentation/UI.java
@@ -1,9 +1,14 @@
 package presentation;
 
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import persistence.DataManager;
+
+import java.util.stream.Collectors;
 
 public class UI
 {
@@ -18,5 +23,23 @@ public class UI
     {
         TextField textField = new TextField();
         Button button = new Button("Add string");
+        button.setOnAction(_ -> {
+            dataManager.addString(textField.getText());
+            textField.setText("");
+        });
+        Label dataShowingLabel = new Label();
+
+        Button displayDataButton = new Button("Show data");
+        displayDataButton.setOnAction(_ -> {
+            String result = dataManager.getStrings().stream().collect(Collectors.joining("\n"));
+            dataShowingLabel.setText(result);
+        });
+
+        VBox box = new VBox(textField, button, displayDataButton, dataShowingLabel);
+        Scene scene = new Scene(box);
+        stage.setScene(scene);
+        stage.setHeight(400);
+        stage.setWidth(400);
+        stage.show();
     }
 }
