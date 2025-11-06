084fe8f132046de9d04d491ad2e35bc04d57832e
Troels Mortensen <tromortensen@hotmail.com>
2025-11-06 07:58:53 +0100

    simple two-layered app structure

--- SimpleAppStructure/src/presentation/UI.java
+++ SimpleAppStructure/src/presentation/UI.java
@@ -1,14 +1,9 @@
 package presentation;
 
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import persistence.DataManager;
-
-import java.util.stream.Collectors;
 
 public class UI
 {
@@ -23,23 +18,5 @@ public class UI
     {
         TextField textField = new TextField();
         Button button = new Button("Add string");
-        button.setOnAction(_ -> {
-            dataManager.addString(textField.getText());
-            textField.setText("");
-        });
-
-        Label output = new Label();
-        Button showData = new Button("Show data");
-        showData.setOnAction(_ ->{
-            String result = dataManager.getStrings().stream().collect(Collectors.joining("\n"));
-            output.setText(result);
-        });
-
-        VBox box = new VBox(textField, button, showData, output);
-        Scene scene = new Scene(box);
-        stage.setScene(scene);
-        stage.setWidth(400);
-        stage.setHeight(400);
-        stage.show();
     }
 }
